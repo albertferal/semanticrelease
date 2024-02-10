fc0d85d8e15631668ee98c5ac42d6ab023272ee7
albertferal <albertferal@gmail.com>
2024-02-10 12:07:02 +0100

    probandoooo

--- ci-utils/update_versions.sh
+++ ci-utils/update_versions.sh
@@ -4,18 +4,18 @@ NEXT_RELEASE_VERSION="${1#"v"}"
 
 function update_version_helm_chart_files() {
   echo "It's going to update appVersion in helm chart to version ${NEXT_RELEASE_VERSION}"
-  yq w -i helm/fast-api-webapp/Chart.yaml "appVersion" "${NEXT_RELEASE_VERSION}"
+  yq w -i charts-counter-app/Chart.yaml "appVersion" "${NEXT_RELEASE_VERSION}"
 
   echo "It's going to update version in helm chart to version ${NEXT_RELEASE_VERSION}"
-  yq w -i helm/fast-api-webapp/Chart.yaml "version" "${NEXT_RELEASE_VERSION}"
+  yq w -i charts-counter-app/Chart.yaml "version" "${NEXT_RELEASE_VERSION}"
 
   echo "It's going to update image tag in helm chart values to version ${NEXT_RELEASE_VERSION}"
-  yq w -i helm/fast-api-webapp/values.yaml "image.tag" "${NEXT_RELEASE_VERSION}"
+  yq w -i charts-counter-app/values.yaml "image.tag" "${NEXT_RELEASE_VERSION}"
 }
 
 function update_helm_docs() {
   echo "It's going to update README.md with helm-docs"
-  helm-docs -c helm/fast-api-webapp/
+  helm-docs -c charts-counter-app/
 }
 
 function update_versions() {
